862277b383e2922449f5e5a988943b31dacfb8b9
Michelle Smith <chellemo23@gmail.com>
2020-04-23 07:15:21 -0400

    Updates

---
+++ precache-manifest.d1175e44964d8d0cd9d8fe26dcc08a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a245f70ded22a6400c2f49a377d01e",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8c1e03ab3f55a04596e0",
+    "url": "/github.io/monsters-rolodex/static/css/main.5d917ea2.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/github.io/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1e03ab3f55a04596e0",
+    "url": "/github.io/monsters-rolodex/static/js/main.6351da1d.chunk.js"
+  },
+  {
+    "revision": "65f0fbb82a8cc6706a2f",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.bdac0139.js"
+  }
+]);
